bc9a3567c238009b78f7d8304e6fdd7b8bab3ae7
Alan Torres <temp_peri@hotmail.com>
2018-04-11 15:23:22 -0500

    About to start something good... :p

--- Eureka.Spe.Core/Tags/Entities/Tag.cs
+++ Eureka.Spe.Core/Tags/Entities/Tag.cs
@@ -5,7 +5,11 @@ namespace Eureka.Spe.Tags.Entities
     public class Tag : FullAuditedEntity
     {
         public string Name { get; set; }
-        public string DisplayName { get; set; }
-        
+        public bool PositiveBehavior { get; set; }
+        public bool PositiveBehaviorValue { get; set; }
+
+        public int StudentId { get; set; }
+        public string EntityName { get; set; }
+        public int EntityId { get; set; }
     }
 }
