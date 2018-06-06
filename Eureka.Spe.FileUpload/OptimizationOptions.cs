9a3d112f6f083e1b6dd618aab3a74199db0c5105
Alan Torres <temp_peri@hotmail.com>
2018-06-06 15:45:05 -0500

    little cleanup

--- Eureka.Spe.FileUpload/OptimizationOptions.cs
+++ Eureka.Spe.FileUpload/OptimizationOptions.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Eureka.Spe.FileUpload
+﻿namespace Eureka.Spe.FileUpload
 {
     public class OptimizationOptions
     {
